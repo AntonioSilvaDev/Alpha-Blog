599d5a0e003bdfff83b9a95e5997e6d1cd53349e
TonyS <tonysilvasd@gmail.com>
2020-01-21 17:19:31 -0800

    add styling for errors

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Will stop the .field-with-errors class from changing the appearance of the form if error exists
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
