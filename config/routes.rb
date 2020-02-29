5df1b9f9bc02b59828482746c076dc05e8b02ec8
TonyS <tonysilvasd@gmail.com>
2020-02-28 16:02:23 -0800

    added user signup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/home'
   get '/about', to: 'welcome#about'
+  get '/signup', to: 'users#new'
 
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles
+  resources :users, except: [:new]
 end
